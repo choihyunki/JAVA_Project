30f1c79e75aca9a5bae1919853670ebd5c59af95
choihyunki <shcoolbus123@gmail.com>
2024-06-10 18:54:38 +0900

    시발ㅈ같노

--- app/src/main/java/com/example/myapplication/ui/toegye/Toegye5fFragment.java
+++ app/src/main/java/com/example/myapplication/ui/toegye/Toegye5fFragment.java
@@ -15,7 +15,7 @@ public class Toegye5fFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
-        return inflater.inflate(R.layout.fragment_yulgok1f, container, false);
+        return inflater.inflate(R.layout.fragment_toegye1f, container, false);
     }
 
 }
--- app/src/main/java/com/example/myapplication/ui/toegye/Toegye7fFragment.java
+++ app/src/main/java/com/example/myapplication/ui/toegye/Toegye7fFragment.java
@@ -15,7 +15,7 @@ public class Toegye7fFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
-        return inflater.inflate(R.layout.fragment_yulgok1f, container, false);
+        return inflater.inflate(R.layout.fragment_toegye1f, container, false);
     }
 
 }
